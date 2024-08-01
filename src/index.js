e0d1365b9a093f3befccd93c69984ef7051bf913
nasta987 <99967637+nasta987@users.noreply.github.com>
2024-08-01 11:32:41 +0300

    feat : update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  n=Math.abs(n);
+  const str = n.toString();
+let reverseStr = "";
+for (let i = 0; i < str.length; i += 1) {
+    reverseStr = `${str[i]}${reverseStr}`;
+} return Number(reverseStr);
 }
